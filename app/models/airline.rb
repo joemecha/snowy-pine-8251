19dd3c961badbf93385f541160cd246e703c9157
joemecha <jwmecha@gmail.com>
2021-04-23 11:32:13 -0500

    Add AR method passenger_list to airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,4 +2,9 @@ class Airline < ApplicationRecord
   has_many :flights
   has_many :flight_passengers, through: :flights
   has_many :passengers, through: :flight_passengers
+
+
+  def passenger_list
+    passengers.where('age >= ?', 18).distinct.pluck(:name)
+  end
 end
